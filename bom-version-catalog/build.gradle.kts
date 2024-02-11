8fb74670b0d5c99a90dd825d819f7f80aaa81450
Ori Yechieli <ori.yechieli@hibob.io>
2024-02-11 16:57:38 +0200

    use gradle jvmToolchain for setting jvm compilation internally rather than external installation

--- bom-version-catalog/build.gradle.kts
+++ bom-version-catalog/build.gradle.kts
@@ -19,15 +19,19 @@ dependencies {
     implementation("net.pearx.kasechange:kasechange:1.4.1")
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
+val javaVersion: JavaVersion = JavaVersion.VERSION_1_8
+val javaMajorVersion: String = javaVersion.majorVersion
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(javaMajorVersion))
     }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "$javaVersion"
+    }
 }
 
 gradlePlugin {
